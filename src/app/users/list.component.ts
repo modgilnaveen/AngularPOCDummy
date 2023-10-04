72107c3ae18a746e07561bcd9a826bf0652862f2
modgilnaveen <146912483+modgilnaveen@users.noreply.github.com>
2023-10-04 18:48:16 +0530

    Add files via upload

--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -6,6 +6,8 @@ import { AccountService } from '../services';
 @Component({ templateUrl: 'list.component.html' })
 export class ListComponent implements OnInit {
     users : any[] = [];
+    searchUser : any[] = [];
+    userList: any[]= [];
 
     constructor(private accountService: AccountService) {}
 
@@ -14,7 +16,8 @@ export class ListComponent implements OnInit {
             .pipe(first())
             .subscribe(users => {
                 let borrowerList = users
-                this.users = borrowerList.filter(x => x.loanAmount !== undefined)
+                this.users = borrowerList.filter(x => x.loanAmount !== undefined);
+                this.userList = [...this.users]
             }) ;
     }
 
@@ -24,7 +27,21 @@ export class ListComponent implements OnInit {
         this.accountService.delete(id)
             .pipe(first())
             .subscribe(() => {
-                this.users = this.users && this.users.filter(x => x.id !== id) 
+                this.users = this.users && this.users.filter(x => x.id !== id);                
             });
+    }
+
+    onInput(e: any) {
+        this.users = this.userList;
+        this.searchUser = this.users && this.users.filter(x => x.firstName === e.target.value ||
+            x.lastName === e.target.value);
+            
+        if(this.searchUser.length > 0 && e.target.value.trim() !== '') {
+            this.users = this.searchUser;
+        } else if (e.target.value.trim() !== '') {
+            this.users = this.searchUser;
+        } else {
+            this.users = this.userList;
+        } 
     }
 }
